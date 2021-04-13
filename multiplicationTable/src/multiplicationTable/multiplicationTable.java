390de71a807251e3e3e17434b393507b34d6ba2b
Haemah Akhtar <ahaemah@gmail.com>
2021-04-13 01:49:11 -0400

    multiplicationTable

---
+++ multiplicationTable/src/multiplicationTable/multiplicationTable.java
@@ -0,0 +1,31 @@
+package multiplicationTable;
+
+
+public class multiplicationTable {
+
+	public static void main(String[] args) {
+		
+		for (int a = 1; a <= 10; a++) 
+		{
+			
+			for (int b = 1; b <= 10; b++) 
+			{
+				
+					System.out.printf("%5d", a*b);
+				
+			}
+				System.out.println();
+				
+			}
+			
+		}
+
+	}
+
+				
+				
+				
+				
+				
+				
+		
